b84bdd82c02219acde714f6d4a20f21609d565c7
whale0928 <rlagusrl928@gmail.com>
2022-11-28 20:31:08 +0900

    feat : Create New Method
    New Method Item Find List

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,16 +14,17 @@ public class ItemRepository {
     private final EntityManager em;
 
 
-    /**아이템 객체 생성
+    /**
+     * 아이템 객체 생성
      * Save.
      *
      * @param item the item
      */
-    public void save(Item item){
+    public void save(Item item) {
         //아이템 아이디가 없으면 아예 새로 만드는 객체
-        if(item.getId() == null) em.persist(item);
-        //있는 경우에는 머지 수행
-        else                     em.merge(item);
+        if (item.getId() == null) em.persist(item);
+            //있는 경우에는 머지 수행
+        else em.merge(item);
     }
 
     /**
@@ -32,10 +34,20 @@ public class ItemRepository {
      * @param id the id
      * @return the item
      */
-    public Item findOne(Long id){
-        return em.find(Item.class,id);
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
     }
 
+
+    /**
+     * 아이템 목록 조회
+     * Find all list.
+     *
+     * @return the list
+     */
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
 
 
 }
