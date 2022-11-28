a28bc1732dd2ae04891b7d864eb10e62071c799d
whale0928 <rlagusrl928@gmail.com>
2022-11-28 20:27:12 +0900

    feat : Create ItemRepository
    New Method Item Save

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+
+    /*˚*아이템 객체 생성
+     * Save.
+     *
+     * @param item the item
+     */
+    public void save(Item item){
+        //아이템 아이디가 없으면 아예 새로 만드는 객체
+        if(item.getId() == null) em.persist(item);
+        //있는 경우에는 머지 수행
+        else                     em.merge(item);
+    }
+
+
+}
