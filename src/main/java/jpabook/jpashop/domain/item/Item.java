9ae5e14d5caf7965d9745b0ba33b8b64b6085088
whale0928 <rlagusrl928@gmail.com>
2022-11-28 19:46:13 +0900

    feat : Create Stock Controller Method
    Create Stock Plus Method
    Create Stock Minus Method
    
    Create new Exception for stock

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,29 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // 구현 비지니스 로직
+
+
+    /** 재고 증가
+     * Add stock.
+     * @param quantity the quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity; //재고 수량 증가
+    }
+
+    /**재고 감소
+     * Remove stock.
+     * @param quantity the quantity
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity-quantity; //재고 수량 증가
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
