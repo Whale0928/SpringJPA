079cb8a0f1c2de4227e214d01d2db99106062c50
whale0928 <rlagusrl928@gmail.com>
2022-11-29 20:01:38 +0900

    feat : Create Method Order Method.
    Create Method( 주문 생성 메서드 )

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,7 +14,8 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
@@ -26,7 +26,7 @@ public class Order {
 
 
     @ManyToOne(fetch = LAZY) //회원과 다대일 관계  여러개의 주문 : 하나의 회원 정보
-    @JoinColumn(name="member_id")
+    @JoinColumn(name = "member_id")
     // 주문과 회원 도메인 어느쪽이 기준(주인)인지 정리해야 한다 ( 기준의 값이 변했으면 다른 값을 변경 )
     private Member member;
 
@@ -65,7 +65,8 @@ public class Order {
     }
 
 
-    /**생성 메서드 -> 복잡한 생성 별도의 생성 전용 메서드가 있는 것이 좋다
+    /**
+     * 생성 메서드 -> 복잡한 생성 별도의 생성 전용 메서드가 있는 것이 좋다
      * Creat order order.
      *
      * @param member     the member
@@ -73,11 +74,11 @@ public class Order {
      * @param orderItems the order items
      * @return the order
      */
-    public static Order creatOrder(Member member, Delivery delivery, OrderItem... orderItems){
+    public static Order creatOrder(Member member, Delivery delivery, OrderItem... orderItems) {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
-        for(OrderItem item : orderItems){
+        for (OrderItem item : orderItems) {
             order.addOrderItem(item);
         }
         order.setStatus(OrderStatus.ORDER);
