82da99eed5387f3b5bbcca03547bf14ece319320
whale0928 <rlagusrl928@gmail.com>
2022-11-30 20:36:15 +0900

    feat : Create order Logic

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    //주문
+    @Transactional
+    public Long order(Long memberId , Long itemId,int count){
+
+        Member orderMember = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(orderMember.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.creatOrder(orderMember, delivery, orderItem);
+        orderRepository.save(order);
+        return order.getId();
+    }
+    //취소
+    //검색
+}
